04e296748547c4faa01ea84f4e8f5da77589522d
mrf-dot <mfeigenbaum23@gmail.com>
2023-04-17 09:33:22 -0400

    Added u8 stuff

---
+++ u8/IndexMakerMap.java
@@ -0,0 +1,113 @@
+// Name:
+// Date:
+
+import java.io.*;
+import java.util.*;
+
+/* This program takes a text file, creates an index (by line numbers)
+ * for all the words in the file and writes the index
+ * into the output file.  The program prompts the user for the file names.
+ */
+public class IndexMakerMap {
+	public static void main(String[] args) throws IOException {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("\nEnter input file name: ");
+		String infileName = keyboard.nextLine().trim();
+		Scanner inputFile = new Scanner(new File(infileName));
+
+		DocumentIndex index = makeIndex(inputFile);
+
+		// System.out.println( index.toString() );
+		PrintWriter outputFile = new PrintWriter(new FileWriter("fishIndex.txt"));
+		outputFile.println(index.toString());
+		inputFile.close();
+		outputFile.close();
+		System.out.println("Done.");
+	}
+
+	public static DocumentIndex makeIndex(Scanner inputFile) {
+		DocumentIndex index = new DocumentIndex();
+		int lineNum = 0;
+		while (inputFile.hasNextLine()) {
+			lineNum++;
+			index.addAllWords(inputFile.nextLine(), lineNum);
+		}
+		return index;
+	}
+}
+
+class DocumentIndex extends TreeMap<String, TreeSet<Integer>> {
+
+	/**
+	 * Makes the word uppercase. If the word is already in the map, updates the
+	 * lineNum.
+	 * Otherwise, adds word and TreeSet to the map, and updates the lineNum
+	 */
+	public void addWord(String word, int lineNum) {
+		if (word.isBlank()) {
+		} else if (this.containsKey((word = word.toUpperCase()))) {
+			this.get(word).add(lineNum);
+		} else {
+			TreeSet<Integer> tmp = new TreeSet<Integer>();
+			tmp.add(lineNum);
+			this.put(word, tmp);
+			lineNum++;
+		}
+	}
+
+	/**
+	 * Extracts all the words from str, skipping punctuation and whitespace
+	 * A good way to skip punctuation and whitespace is to use String's
+	 * split method, e.g., split("[., \"!?]") and assign it to an array of words
+	 * For each word in the array calls addWord(word, num)
+	 */
+	public void addAllWords(String str, int lineNum) {
+		for (String word : str.split("[., \"!?]"))
+			addWord(word, lineNum);
+	}
+
+	public String toString() {
+		String toRet = "";
+		for (String s : this.keySet()) {
+			toRet += s + " "; // String maps to
+			for (Integer i : this.get(s)) // TreeSet<Integer>
+				toRet += i + ", ";
+			toRet = toRet.substring(0, toRet.length() - 2);
+			toRet += "\n";
+		}
+		return toRet;
+	}
+}
+
+/**********************************************
+ * ----jGRASP exec: java -ea IndexMakerMap
+ *
+ * Enter input file name: fish.txt
+ * Done.
+ *
+ * ----jGRASP: operation complete.
+ *
+ ************************************************/
+/******************
+ * fishIndex.txt **************
+ * A 12, 14, 15
+ * ARE 16
+ * BLACK 6
+ * BLUE 4, 7
+ * CAR 14
+ * FISH 1, 2, 3, 4, 6, 7, 8, 9, 16
+ * HAS 11, 14
+ * LITTLE 12, 14
+ * LOT 15
+ * NEW 9
+ * OF 16
+ * OLD 8
+ * ONE 1, 11, 14
+ * RED 3
+ * SAY 15
+ * STAR 12
+ * THERE 16
+ * THIS 11, 14
+ * TWO 2
+ * WHAT 15
+ ************************/
---
+++ u8/PolynomialLab.java
@@ -0,0 +1,129 @@
+// Name:    
+// Date: 
+
+import java.util.*;
+public class PolynomialLab {
+	public static void main(String[] args) {
+		Polynomial poly = new Polynomial(); // 2x^3 + -4x + 2
+		poly.makeTerm(1, -4);
+		poly.makeTerm(3, 2);
+		poly.makeTerm(0, 2);
+		System.out.println("Map:  " + poly.getMap());
+		System.out.println("String:  " + poly.toString());
+		double evaluateAt = 2.0;
+		System.out.println("Evaluated at " + evaluateAt + ": " + poly.evaluateAt(evaluateAt));
+
+		System.out.println("-----------");
+		Polynomial poly2 = new Polynomial(); // 2x^4 + x^2 + -5x + -3
+		poly2.makeTerm(1, -5);
+		poly2.makeTerm(4, 2);
+		poly2.makeTerm(0, -3);
+		poly2.makeTerm(2, 1);
+		System.out.println("Map:  " + poly2.getMap());
+		System.out.println("String:  " + poly2.toString());
+		evaluateAt = -10.5;
+		System.out.println("Evaluated at " + evaluateAt + ": " + poly2.evaluateAt(evaluateAt));
+
+		System.out.println("-----------");
+		System.out.println("Sum: " + poly.add(poly2));
+		System.out.println("Product:  " + poly.multiply(poly2));
+
+	}
+}
+
+class Polynomial {
+	private Map<Integer, Integer> poly;
+
+	public Polynomial() {
+		poly = new TreeMap();
+	}
+
+	public void makeTerm(Integer exp, Integer coef) {
+		poly.put(exp, coef);
+	}
+
+	public Map<Integer, Integer> getMap() {
+		return poly;
+	}
+
+	public double evaluateAt(double x) {
+		double sum = 0;
+		for (Integer i : poly.keySet())
+			sum += poly.get(i) * Math.pow(x, i);
+		return sum;
+	}
+
+	public Polynomial add(Polynomial other) {
+		Polynomial sum = new Polynomial();
+		Map<Integer, Integer> polyMap = this.poly;
+		Map<Integer, Integer> otherMap = other.poly;
+		// finish code here:
+		for (Integer i : polyMap.keySet()) {
+			if (otherMap.containsKey(i))
+				sum.makeTerm(i, polyMap.get(i) + otherMap.get(i));
+			else
+				sum.makeTerm(i, polyMap.get(i));
+		}
+		for (Integer i : otherMap.keySet())
+			if (!sum.getMap().containsKey(i))
+				sum.makeTerm(i, otherMap.get(i));
+		return sum;
+	}
+
+	public Polynomial multiply(Polynomial other) {
+		Polynomial prod = new Polynomial();
+		int exp, coeff;
+		for (Integer i : poly.keySet()) {
+			for (Integer j : other.getMap().keySet()) {
+				exp = i + j;
+				coeff = other.getMap().get(j) * poly.get(i);
+				if (prod.getMap().containsKey(exp))
+					coeff += prod.getMap().get(exp);
+				prod.makeTerm(exp, coeff);
+			}
+		}
+		return prod;
+	}
+
+	public String toString() {
+		Set<Integer> st = poly.keySet();
+		Iterator<Integer> iter = st.iterator();
+		String s = "";
+		while (iter.hasNext()) {
+			Integer exp = iter.next();
+			Integer coef = poly.get(exp);
+			if (exp == 0) // constant term
+				s = coef + "";
+			else if (coef == 0) // 0x
+				s = s;
+			else if (exp == 1) // x^1 term
+			{
+				if (coef == 1) // 1x^1
+					s = "x + " + s;
+				else
+					s = coef + "x" + " + " + s; // 3x^1
+			} else if (coef == 1) // 1x
+				s = "x^" + exp + " + " + s;
+			else
+				s = coef + "x^" + exp + " + " + s; // insert at the front.
+		}
+		return s;
+	}
+
+}
+
+/***************************************
+ * ----jGRASP exec: java PolynomialLab
+ * Map: {0=2, 1=-4, 3=2}
+ * String: 2x^3 + -4x + 2
+ * Evaluated at 2.0: 10.0
+ * -----------
+ * Map: {0=-3, 1=-5, 2=1, 4=2}
+ * String: 2x^4 + x^2 + -5x + -3
+ * Evaluated at -10.5: 24469.875
+ * -----------
+ * Sum: 2x^4 + 2x^3 + x^2 + -9x + -1
+ * Product: 4x^7 + -6x^5 + -6x^4 + -10x^3 + 22x^2 + 2x + -6
+ * 
+ * ----jGRASP: operation complete.
+ ********************************************/
